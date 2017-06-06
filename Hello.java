bdbf7c5ebd27486be56352fdb342b39dfcacba7b
Sakthivelgovindan <sakthivelit059@gmail.com>
2017-06-06 15:01:48 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Hello {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		//int b=sc.nextInt();
+		//int c=sc.nextInt();
+		for(int i=0;i<a;i++)
+		{
+		    System.out.println("Hello");
+		}
+	}
+}
