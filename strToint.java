f58c0ab6ac451b93f7722813a6cddcca4fd73cab
Sakthivelgovindan <sakthivelit059@gmail.com>
2017-06-07 15:04:10 +0530

    Create strToint.java

---
+++ strToint.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class strToint{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        int b=Integer.valueOf(a);
+        System.out.println(b);
+    }
+}
