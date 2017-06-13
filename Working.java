c6b1ca85a58ad1496c3a51e69dee4800dadf5b49
Sakthivelgovindan <sakthivelit059@gmail.com>
2017-06-13 19:06:59 +0530

    Create Working.java

---
+++ Working.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Working {
+	public static void main (String[] args) {
+	Scanner sc=new Scanner(System.in);
+	String a=sc.nextLine();
+	switch(a)
+	{
+	    case "sunday":
+	    {
+	        System.out.print("false");
+	        break;
+	    }
+	    default:
+	    {
+	        System.out.print("true");
+	        break;
+	    }
+	}
+	}
+}
